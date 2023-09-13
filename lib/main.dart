c2d698be4e3762e690bbdf7bb8d07718503a8c9a
Xiongxinjiang <xiongxinjiang@fiture.com>
2023-09-13 18:36:44 +0800

    444

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:catcher/catcher.dart';
 import 'package:device_preview/device_preview.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'dart:collection';
 
 import 'src/app/routers/router.dart';
 
